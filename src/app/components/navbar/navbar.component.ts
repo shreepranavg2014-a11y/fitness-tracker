8c79f3b604aea5b0469bb5ed32ad702ab7c0fb21
shreepranavg2014-a11y <shreepranavg2014@gmail.com>
2026-01-16 12:12:56 +0530

    Update navbar.component.ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,22 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  isAuthenticated$ = this.authService.isAuthenticated$;
-
-  constructor(
-    private router: Router,
-    private authService: AuthService
-  ) {}
-
-  logout(): void {
-    this.authService.logout();
-    this.router.navigate(['/workouts']);
-  }
+  title = 'Fitness Tracker';
 }
