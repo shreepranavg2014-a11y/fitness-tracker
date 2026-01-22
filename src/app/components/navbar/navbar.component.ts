d8c39f5c6c1fcb01c68bd33834951fb955802f77
shreepranavg2014-a11y <shreepranavg2014@gmail.com>
2026-01-22 12:16:33 +0530

    Update navbar.component.ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
+  standalone: false,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   title = 'Fitness Tracker';
-    logout() { console.log('Logout clicked'); }
 }
