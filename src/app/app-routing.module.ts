3c414d7acd9a3bc1f3767d6ad24a3630577cb46c
shreepranavg2014-a11y <shreepranavg2014@gmail.com>
2026-01-22 21:29:41 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WorkoutListComponent } from './components/workout-list/workout-list.component';
+import { WorkoutsComponent } from './components/workouts/workouts.component';
 import { WorkoutDetailComponent } from './components/workout-detail/workout-detail.component';
 import { ProgressTrackerComponent } from './components/progress-tracker/progress-tracker.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 export const appRoutes: Routes = [
-  {
-    path: '',
-    redirectTo: 'workouts',
-    pathMatch: 'full'
-  },
-  {
-    path: 'workouts',
-    component: WorkoutListComponent,
-    data: { title: 'All Workouts' }
-  },
-  {
-    path: 'workouts/:id',
-    component: WorkoutDetailComponent,
-    data: { title: 'Workout Details' }
-  },
-  {
-    path: 'progress',
-    component: ProgressTrackerComponent,
-    data: { title: 'Progress Tracker' }
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
-    data: { title: 'User Profile' }
-  },
-  {
-    path: '**',
-    redirectTo: 'workouts',
-    pathMatch: 'full'
-  }
+  { path: '', redirectTo: 'workouts', pathMatch: 'full' },
+  { path: 'workouts', component: WorkoutsComponent, data: { title: 'All Workouts' } },
+  { path: 'workouts/:id', component: WorkoutDetailComponent, data: { title: 'Workout Details' } },
+  { path: 'progress', component: ProgressTrackerComponent, data: { title: 'Progress Tracker' } },
+  { path: 'profile', component: ProfileComponent, data: { title: 'User Profile' } }
 ];
 
 @NgModule({
